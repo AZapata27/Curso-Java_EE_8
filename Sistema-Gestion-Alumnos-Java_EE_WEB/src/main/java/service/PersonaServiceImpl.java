f6e4e5089c5fec214d7701f273fe2509ca5d53d5
Andres Zapata <azapata@uc.edu.co>
2021-04-23 11:25:57 -0500

    Primer Commit

---
+++ Sistema-Gestion-Alumnos-Java_EE_WEB/src/main/java/service/PersonaServiceImpl.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import data.PersonaDao;
+import domain.Persona;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+@Stateless
+public class PersonaServiceImpl implements PersonaService{
+    
+   @Inject
+   private PersonaDao personaDao;
+
+    @Override
+    public List<Persona> listarPersonas() {
+        return personaDao.findAllPersonas();
+    }
+
+    @Override
+    public Persona encontrarPersonaPorId(Persona persona) {
+        return personaDao.findPersonaById(persona);
+    }
+
+    @Override
+    public Persona encontrarPersonaPorEmail(Persona persona) {
+        return personaDao.findPersonaByEmail(persona);
+    }
+
+    @Override
+    public void registrarPersona(Persona persona) {
+       personaDao.insertPersona(persona);
+    }
+
+    @Override
+    public void modificarPersona(Persona persona) {
+        personaDao.updatePersona(persona);
+    }
+
+    @Override
+    public void eliminarPersona(Persona persona) {
+       personaDao.deletePersona(persona);
+    }
+    
+}
---
+++ Sistema-Gestion-Alumnos-Java_EE_WEB/src/main/java/service/UsuarioService.java
@@ -0,0 +1,23 @@
+
+package service;
+
+import domain.Usuario;
+import java.util.List;
+import javax.ejb.Local;
+
+@Local
+public interface UsuarioService {
+    
+    public List<Usuario> listarUsuarios();
+    
+    public Usuario encontrarPersonaPorId(Usuario usuario);
+    
+    public Usuario encontrarUsuarioPorUsername(Usuario usuario);
+    
+    public void registrarUsuario(Usuario usuario);
+    
+    public void modificarUsuario(Usuario usuario);
+    
+    public void eliminarUsuario(Usuario usuario);
+    
+}
